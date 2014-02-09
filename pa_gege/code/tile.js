0df45382b2d25c17d69e78b560de5ea7e671a3b4
Georg Grab <talkdirty@users.noreply.github.com>
2014-02-09 21:42:02 +0100

    old

--- pa_gege/code/tile.js
+++ pa_gege/code/tile.js
@@ -1,10 +1,6 @@
 // TODO various performance improvements
 var TileDispatcher = (function(){
     var _tileid;
-    var _tileobj;
-    var interval;
-    var IMAGE_HANDLER = 0x00001;
-    
     function withResponseObject(url, callback){
         $.ajax({
             url:url,
@@ -14,67 +10,77 @@ var TileDispatcher = (function(){
         });
     }
     
-    function TileDispatcher(tileObj, tileID){
-        _tileid = tileID;
-        _tileobj = tileObj;
+    function TileDispatcher(tileID){
+        this._tileid = tileID;
+        this._ScaleCount = 0;
+        this._interval = null;
+        this._resolv = [["Über uns", "uberuns"], ["Stiftung", "stiftung"],["Presse", "presse"],["Unterstützen","unterstutzen"],["Shop", "shop"],["Kalender", "cal"],["Bilder","bilder"]]
+
     }
     
     TileDispatcher.prototype.getOnClickHandler = function(){
         return function(){
-            load();
+            console.log("Tile Click: id="+ this._tileid);
+            if (this._tileid > 7){
+                this._tileid = this._tileid - 7;
+            }
+            _tileid--;
+            load(this._resolv[this._tileid][0], this._resolv[this._tileid][1]);
         };
     };
     
-    function load(){
-        interval = setInterval(sclSmaller, 10);
+    function load(prettyWhat, URLWhat){
+        this._interval = setInterval(sclSmaller, 10);
         setTimeout(activateLoadingAnimation, 30);
-        $("#result").load("content/bilder.html", function(){
-           withResponseObject("http://0.0.0.0/handler/images/num", function(retobj){
-                var tcolcount = 0;    
-                for(var i = 0; i <= Math.ceil(retobj.numtiles / 5); i++){
-                   $("#scrolledcontentoff").append(
-                       "<div class=\"tile-column\" id=\"imgcol-"+ i +"\"></div>"
-                   );
-                   for (var x = 0; x <= 4; x++){
-                       $("#imgcol-"+i).append(
-                           $("<div>").addClass("stdtile x-"+x).append(
-                               $("<div>").addClass("tile-scaling"),
-                               $("<div>").addClass("tile-content test-b").append(
-                                   $("<a>").attr("href", "#")
-                               )
-                           )
-                       );
-                   }
-                }
-           });
+        $("#result").load("content/"+ URLWhat +".html", function(){
+            setTimeout("load_" + URLWhat, 0);
+        });
         $(".ctitle").fadeTo(200,0);
-        $.getScript("content/bilder.js", function(){
-           $.scrollTo("#scrolled", 800, {onAfter: function(){
-               $(".ctitle").html("Chorknaben // Bilder");
-               $(".ctitle").fadeTo(200,1);
-               // Revert loading Animation
-               $("#loading-img").css({visibility:"hidden"});
-               $("#header-img").width(90);
-               ScaleCount = 0;
-           }});
-      });
-      });
+        $.scrollTo("#scrolled", 800, {onAfter: function(){
+            $(".ctitle").html("Chorknaben // " + prettyWhat);
+            $(".ctitle").fadeTo(200,1);
+            // Revert loading Animation
+            $("#loading-img").css({visibility:"hidden"});
+            $("#header-img").width(90);
+            this._ScaleCount = 0;
+        }});
+    }
+
+    function load_bilder(){
+        withResponseObject("http://0.0.0.0/handler/images/num", function(retobj){
+             var tcolcount = 0;    
+             for(var i = 0; i <= Math.ceil(retobj.numtiles / 5); i++){
+                $("#scrolledcontentoff").append(
+                    "<div class=\"tile-column\" id=\"imgcol-"+ i +"\"></div>"
+                );
+                for (var x = 0; x <= 4; x++){
+                    $("#imgcol-"+i).append(
+                        $("<div>").addClass("stdtile x-"+x).append(
+                            $("<div>").addClass("tile-scaling"),
+                            $("<div>").addClass("tile-content test-b").append(
+                                $("<a>").attr("href", "#")
+                            )
+                        )
+                    );
+                }
+                $("#scrolledcontentoff").pullupScroll("#scrolledcontentoff .tile-column");
+             }
+        });
     }
     
     function activateLoadingAnimation(){
         $("#loading-img").css({visibility:"visible"});
     }
     
-    var ScaleCount = 0;
     function sclSmaller(){
         console.log("ohai");
-        if (ScaleCount >= 20){
-            window.clearInterval(interval);
+        if (this._ScaleCount >= 20){
+            window.clearInterval(this._interval);
             return;
         }    
-        ScaleCount++;
+        this._ScaleCount++;
         var HeaderImg = $("#header-img");
-        HeaderImg.width(90 - ScaleCount);
+        HeaderImg.width(90 - this._ScaleCount);
     }
     
     return TileDispatcher;
