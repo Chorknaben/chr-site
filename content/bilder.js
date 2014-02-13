2be921369f03c937d0557ea752df0828443c4358
Georg Grab <talkdirty@users.noreply.github.com>
2014-02-13 17:59:24 +0100

    document.ready() working thumbnail loading implementation

--- content/bilder.js
+++ content/bilder.js
@@ -1,1 +1,9 @@
+$(document).ready(function(){
+    $('.image-thumb').each(function(i){
+        //$(Obj).css({background-image:'/images/thumbs/'+i});
+        //$(Obj).css({background-image:'/images/thumbs/'+i});
+        console.log(this);
+        $(this).css({'background-image':'url(/images/thumbs/'+(i+1)+')'});
+    })
+});
 
