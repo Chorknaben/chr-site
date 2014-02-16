86ddc37fb5e4b76840bd1ea6493e24c2ffe05c74
Georg Grab <talkdirty@users.noreply.github.com>
2014-02-16 16:16:07 +0100

    Commenting / Refactoring / Various improvements

--- code/ready.js
+++ code/ready.js
@@ -1,16 +1,6 @@
-var headerVisible = false;
-var navVisible = false;
-var isScrolledDown = false;
 var SELECTOR_TILE = '.tile-content';
 var SELECTOR_NAV = '.navitem div';
-var animating = false;
-
-/*document.ready = function(){
-    $(SELECTOR_TILE + ", " + SELECTOR_NAV).blurjs({
-        radius:'25',
-        source:'body'
-    });
-}*/
+var _isScrolledDown = false;
 
 (function ( $ ) {
     $.fn.pullupScroll = function(e) {
@@ -48,53 +38,74 @@ function scrollFn() {
 }
 
 $(document).ready(function(){
-    var width = $(window).width();
-    var height = $(window).height();
-    var bgSrc = "/" + width + "/" + (height - 90) + "/";
+    /* Es wird die Bildschirmauflösung ermittelt und an den Server geschickt.
+     * Der Server gibt dann ein passendes Hintergrundbild zurück.
+     * */
+    var width = $(window).width(),
+        height = $(window).height(),
+        bgSrc = "/" + width + "/" + (height - 90) + "/";
     console.log(bgSrc);
-    var img = $('<img>', {
+    // Hinzufügen des Hintergrundbilds zum DOM
+    $('<img>', {
         src:bgSrc + "bg"
     }).hide().appendTo($('#bg')).load(function(){
         $(this).remove(); 
         $('#bg').css('background-image', 'url(' + bgSrc +'bg)').fadeIn(300);
     });
+    /* Jetzt werden basierend auf der Bildschirmauflösung die Hintergrundbilder
+     * der Kacheln angefordert.
+     * Das indexes Array stimmt mit den serverseitig gelagerten Kachel-Festlegungen überein. (Siehe src/Site.hs)
+     * */
+    // Array wird populiert
     var indexes = new Array();
-    for (var i=1; i<=12; i++) {indexes[i] = i + "";}
+    for (var i=1; i<=12; i++) indexes[i] = i + "";
     indexes[7] = "bigtile-content";
     indexes[12] = "nav-last";
+    // Bilder werden hinzugefügt
     for (var m=1; m<=12; m++){
         $("#"+indexes[m]).css('background-image','url('+bgSrc+m+')');
+        // Onclick-Handler werden zu den Kacheln hinzugefügt (->tile.js)
         $("#"+indexes[m]).click(tileOnClickHandler(m));
     }
-    $(SELECTOR_NAV).not('.hoveroverlay').each(function(index,Obj){
+    /* Der Hover-Effekt wird zu allen Kacheln hinzugefügt
+     * */
+    Stl = $(SELECTOR_TILE);
+    Snv = $(SELECTOR_NAV);
+    Snv.not('.hoveroverlay').each(function(index,Obj){
         // Add Hover Effect
-        $(Obj).hover(function(){
-            $(Obj).children(".hoveroverlay").animate({opacity:'0.7'}, 100);
-            $($(SELECTOR_TILE)[index+1]).children('a').children('.hoveroverlay').animate({opacity:'0.7'},100);
+        Obj = $(Obj);
+        Obj.hover(function(){
+            // Maus bewegt sich in eine Navigations Kachel
+            Obj.children(".hoveroverlay").animate({opacity:'0.7'}, 100);
+            $(Stl[index+1]).children('a').children('.hoveroverlay').animate({opacity:'0.7'},100);
         }, function(){
-            $(Obj).children(".hoveroverlay").animate({opacity:'0'}, 200);
-            $($(SELECTOR_TILE)[index+1]).children('a').children('.hoveroverlay').animate({opacity:'0'},200);
+            // Maus bewegt aus einer Navigations Kachel heraus
+            Obj.children(".hoveroverlay").animate({opacity:'0'}, 200);
+            $(Stl[index+1]).children('a').children('.hoveroverlay').animate({opacity:'0'},200);
         });
     });
-    $(SELECTOR_TILE).each(function(index,Obj){
-            $(Obj).hover(function(){
-                $(Obj).children('a').children('.hoveroverlay').animate({opacity:'0.7'},100);
-                $($(SELECTOR_NAV).not('.hoveroverlay')[index-1]).children(".hoveroverlay").animate({opacity:'0.7'}, 100);
-            }, function(){
-                $(Obj).children('a').children('.hoveroverlay').animate({opacity:'0'},100);
-                $($(SELECTOR_NAV).not('.hoveroverlay')[index-1]).children(".hoveroverlay").animate({opacity:'0'}, 100);
-            });
-        $(Obj).hover(function(){
-            $(Obj).children('a').children('.hoveroverlay').animate({opacity:'0.7'},100);
-            $($(SELECTOR_NAV).not('.hoveroverlay')[index-1]).children(".hoveroverlay").animate({opacity:'0.7'}, 100);
+    Stl.each(function(index,Obj){
+        Obj = $(Obj);
+        Obj.hover(function(){
+            // Maus bewegt sich in eine Content Kachel
+            Obj.children('a').children('.hoveroverlay').animate({opacity:'0.7'},100);
+            $(Snv.not('.hoveroverlay')[index-1]).children(".hoveroverlay").animate({opacity:'0.7'}, 100);
         }, function(){
-            $(Obj).children('a').children('.hoveroverlay').animate({opacity:'0'},100);
-            $($(SELECTOR_NAV).not('.hoveroverlay')[index-1]).children(".hoveroverlay").animate({opacity:'0'}, 100);
+            // Maus bewegt sich aus einer Content Kachel heraus
+            Obj.children('a').children('.hoveroverlay').animate({opacity:'0'},100);
+            $(Snv.not('.hoveroverlay')[index-1]).children(".hoveroverlay").animate({opacity:'0'}, 100);
         });
     });
+    /* Es gibt einige Sachen, die beim scrollen erledigt werden müssen
+     * */
     $(window).scroll(function(){
+        // Zum einen der Scoll-Handler von jQuery's pullupScroll für die Bilder
+        // ([TODO]Outsource ->bilder.js?)
         scrollFn();
+        // Zum anderen die nachfolgenden selbst definierten Events
         if ($(this).scrollTop() == 0 && $('.ctitle').html() != "St.-Martins-Chorknaben Biberach"){
+            // Wenn nach dem Besuch einer Seite wieder nach oben gescrollt wird,
+            // wird nach einiger Zeit der Titel wieder zu "St.-Martins-Chorknaben Biberach"
             setTimeout(function(){
                 if ($(window).scrollTop() == 0){
                     window.location.hash = "#";
@@ -106,36 +117,32 @@ $(document).ready(function(){
                 } 
             }, 1000);
         }
-        if ($(this).scrollTop() > 140 && !navVisible){
-            navVisible = true;
-        } 
-        
-        if ($(this).scrollTop() < 140 && navVisible){
-            navVisible = false;
-        }
       
+        // Die nachfolgende Bedingung evaluiert zu true, wenn die angeforderte Seite
+        // vollständig geladen ist
+        // ([TODO] auf überholtheit aufgrund callback in tile.js überprüfen)
         if ($(this).scrollTop() > $(window).height() - 40 && !isScrolledDown){
-           $(".header-nav").fadeTo(200,1);
-           if (CURRENTLY_LOADED_URL != "null"){
-               window.location.hash = CURRENTLY_SCROLLED_URL;
-           }
-           isScrolledDown = true;
-           if (CURRENTLY_LOADED != "null"){
-               $(".ctitle").fadeTo(200,0);
-               setTimeout(function(){
-                   $(".ctitle").html("Chorknaben // " + CURRENTLY_LOADED);
-                   $(".ctitle").fadeTo(200,1);
-               },200);
+            // Navigation einblenden
+            // ([TODO] trotz unsichtbar: klickbar -> fix)
+            $(".header-nav").fadeTo(200,1);
+            if (CURRENTLY_LOADED_URL != "null"){
+                window.location.hash = CURRENTLY_LOADED_URL;
             }
-      }
-      
+            isScrolledDown = true;
+            // ([TODO] titel soll nach mehrmaligen partiellen hochscrollen nicht neu eingefaded werden)
+            if (CURRENTLY_LOADED != "null"){
+                $(".ctitle").fadeTo(200,0);
+                setTimeout(function(){
+                    $(".ctitle").html("Chorknaben // " + CURRENTLY_LOADED);
+                    $(".ctitle").fadeTo(200,1);
+                },200);
+            }
+        }
+
+        // Umkehrfunktion obiger if-anweisung
         if ($(this).scrollTop() < $(window).height() - 40 && isScrolledDown){
            $(".header-nav").fadeTo(200,0);
            isScrolledDown = false;
-      }
+       }
     });
 });
-
-var fadeTo = function(obj, toBG, toText){
-    $(obj).animate({backgroundColor : toBG}, 240);
-}
